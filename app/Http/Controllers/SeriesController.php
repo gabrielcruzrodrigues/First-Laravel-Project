d54956cf0c7949a87f67f857a6f4c4b1ce12690b
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-09-23 08:56:34 -0300

    feat: Criando components no Blade

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -10,6 +10,6 @@ class SeriesController extends Controller
     public function index() 
     {
         $series = ['Serie 1', 'Serie 2', 'Serie 3'];
-        return view('ListarSeries')->with('series', $series);
+        return view('series.index')->with('series', $series);
     }
 }
