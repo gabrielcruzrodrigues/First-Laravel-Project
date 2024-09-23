7e1c950a0bd4862b1d480fd52fc3545d159c3051
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-09-23 07:49:02 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    
+    public function index() 
+    {
+        $series = ['Serie 1', 'Serie 2', 'Serie 3'];
+        return view('ListarSeries', compact('series'));
+    }
+}
